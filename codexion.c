5cc4121ddbe849d8ca75518df969c695bc6597ce
Takaya Enomoto <tenomoto@c4r3s5.42tokyo.jp>
2026-06-22 16:43:31 +0900

    feat:最低限のmain関数を作成
    - コマンドライン引数を受け取って1つ目の文字列と引数の数を
    printする最低限のMVP

---
+++ codexion.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tenomoto <tenomoto@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 15:00:20 by tenomoto          #+#    #+#             */
+/*   Updated: 2026/06/22 16:39:32 by tenomoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	printf("This is cmdline str:%s\n", av[1]);
+	printf("This is cmdline count:%d\n", ac);
+	return (0);
+}
