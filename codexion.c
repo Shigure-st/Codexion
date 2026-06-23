90f30e68fa95ceabf26a096e1c204b8e4fd35363
Takaya Enomoto <tenomoto@c4r3s5.42tokyo.jp>
2026-06-23 17:46:27 +0900

    feat:step3 use mutex

--- codexion.c
+++ codexion.c
@@ -15,19 +15,22 @@
 #include <pthread.h>
 
 int wallet = 0;
+pthread_mutex_t wallet_lock;
 
 void *add_money(void* arg)
 {
 	for (int i = 0; i < 100000; i++)
 	{
+		pthread_mutex_lock(&wallet_lock);
 		wallet++;
+		pthread_mutex_unlock(&wallet_lock);
 	}
 	return NULL;
 }
 int	main()
 {
 	pthread_t	t1, t2;
-	printf("[main]employ sub_thread... \n");
+	pthread_mutex_init(&wallet_lock, NULL);
 
 	pthread_create(&t1, NULL, add_money, NULL);
 	pthread_create(&t2, NULL, add_money, NULL);
@@ -35,6 +38,7 @@ int	main()
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
 
-	printf("finall wallet in: %d yen\n", wallet);
+	pthread_mutex_destroy(&wallet_lock);
+	printf("[FIX]finall wallet in: %d yen\n", wallet);
 	return (0);
 }
