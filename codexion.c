629cd1dab900e085ecca52c22c918513efd1d5d4
Takaya Enomoto <tenomoto@c4r3s5.42tokyo.jp>
2026-06-25 14:29:14 +0900

    feat:check pthread_cond func

--- codexion.c
+++ codexion.c
@@ -14,37 +14,56 @@
 #include <unistd.h>
 #include <pthread.h>
 
-int wallet = 0;
-pthread_mutex_t wallet_lock;
+int ramen_ready = 0;
+pthread_mutex_t kitchen_lock;
+pthread_cond_t ramen_cond;
 
-void *add_money(void* arg)
+void *customer(void* arg)
 {
-	printf("arg: %d \n", *(int*)arg);
-	for (int i = 0; i < 100000; i++)
+	char	*name = (char*)arg;
+
+	pthread_mutex_lock(&kitchen_lock);
+	while (ramen_ready == 0)
 	{
-		pthread_mutex_lock(&wallet_lock);
-		wallet++;
-		pthread_mutex_unlock(&wallet_lock);
+		printf("[%s] waiting for ramen... \n", name);
+		// pthread_cond_wait(&ramen_cond, &kitchen_lock);
 	}
+	printf("[%s] oh, coming ramen\n", name);
+
+	pthread_mutex_unlock(&kitchen_lock);
+	return NULL;
+}
+
+void *chef(void* arg)
+{
+	printf("[staff] I start to make ramen....\n");
+	sleep(5);
+
+	pthread_mutex_lock(&kitchen_lock);
+	ramen_ready = 1;
+	printf("[staff] welcome ramen\n");
+	pthread_cond_broadcast(&ramen_cond);
+	pthread_mutex_unlock(&kitchen_lock);
 	return NULL;
 }
 int	main()
 {
-	pthread_t	t1, t2, t3;
-	pthread_mutex_init(&wallet_lock, NULL);
-	int t1_value = 100;
-	int t2_value = 300;
-	int t3_value = 400;
+	pthread_t	t_chef, t_customer1, t_customer2;
 
-	pthread_create(&t1, NULL, add_money, (void*)&t1_value);
-	pthread_create(&t2, NULL, add_money, (void*)&t2_value);
-	pthread_create(&t3, NULL, add_money, (void*)&t3_value);
+	pthread_mutex_init(&kitchen_lock, NULL);
+	pthread_cond_init(&ramen_cond, NULL);
 
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
-	pthread_join(t3, NULL);
+	pthread_create(&t_customer1, NULL, customer, "Kyaku:A");
+	pthread_create(&t_customer2, NULL, customer, "Kyaku:B");
+	sleep(1);
+	pthread_create(&t_chef, NULL, chef, NULL);
 
-	pthread_mutex_destroy(&wallet_lock);
-	printf("[FIX]finall wallet in: %d yen\n", wallet);
+	pthread_join(t_customer1, NULL);
+	pthread_join(t_customer2, NULL);
+	pthread_join(t_chef, NULL);
+
+	pthread_mutex_destroy(&kitchen_lock);
+	pthread_cond_destroy(&ramen_cond);
+	printf("[main]finished sales\n");
 	return (0);
 }
