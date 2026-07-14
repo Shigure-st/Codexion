d7835bcf76e273ad0c4bcb6a109ddcf3c14c9715
Takaya Enomoto <tenomoto@c4r3s1.42tokyo.jp>
2026-07-14 18:05:17 +0900

    feat(codexion): コーダーとドングルの構造体定義の修正および仮初期化の実装
    - `t_SharedContext`、`t_Coder`、`t_Dongle`
    の前方宣言と構造体定義を追加・修正しました。
    - 以前記述されていたラーメン店シミュレーション用の仮コード（`chef`,
    `customer` など）をコメントアウト
    -`main`関数内に、2名のコーダーと2つのドングルの初期化および
    デバッグ出力処理を追加しました。

--- codexion.c
+++ codexion.c
@@ -15,77 +15,102 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-typedef struct s_SharedContext
+typedef struct s_SharedContext t_SharedContext;
+typedef struct s_Coder t_Coder;
+typedef struct s_Dongle t_Dongle;
+
+struct s_SharedContext
 {
 	int			number_of_coders;
 	int			time_to_debug;
 	int			time_to_refactor;
 	int			time_to_compile;
 	bool		is_active;
-	t_dongle	*dongle_array;
-	t_coder		*coder_array;
-}	t_SharedContext;
+	t_Dongle	*dongle_array;
+	t_Coder		*coder_array;
+};
 
-typedef struct s_Dongle
+struct s_Dongle
 {
 	pthread_mutex_t dongle_lock;
-}	t_Dongle
+};
 
-typedef struct s_Coder
+struct s_Coder
 {
 	int				number;
 	struct s_Dongle	*right_hand_dongle;
 	struct s_Dongle	*left_hand_dongle;
 	struct s_SharedContext	*shared_ctx;
-}	t_Coder;
+};
 
-void *customer(void* arg)
-{
-	char	*name = (char*)arg;
+// void *customer(void* arg)
+// {
+// 	char	*name = (char*)arg;
+//
+// 	pthread_mutex_lock(&kitchen_lock);
+// 	while (ramen_ready == 0)
+// 	{
+// 		printf("[%s] waiting for ramen... \n", name);
+// 		// pthread_cond_wait(&ramen_cond, &kitchen_lock);
+// 	}
+// 	printf("[%s] oh, coming ramen\n", name);
+//
+// 	pthread_mutex_unlock(&kitchen_lock);
+// 	return NULL;
+// }
+//
+// void *chef(void* arg)
+// {
+// 	printf("[staff] I start to make ramen....\n");
+// 	sleep(5);
+//
+// 	pthread_mutex_lock(&kitchen_lock);
+// 	ramen_ready = 1;
+// 	printf("[staff] welcome ramen\n");
+// 	pthread_cond_broadcast(&ramen_cond);
+// 	pthread_mutex_unlock(&kitchen_lock);
+// 	return NULL;
+// }
 
-	pthread_mutex_lock(&kitchen_lock);
-	while (ramen_ready == 0)
-	{
-		printf("[%s] waiting for ramen... \n", name);
-		// pthread_cond_wait(&ramen_cond, &kitchen_lock);
-	}
-	printf("[%s] oh, coming ramen\n", name);
-
-	pthread_mutex_unlock(&kitchen_lock);
-	return NULL;
-}
-
-void *chef(void* arg)
-{
-	printf("[staff] I start to make ramen....\n");
-	sleep(5);
-
-	pthread_mutex_lock(&kitchen_lock);
-	ramen_ready = 1;
-	printf("[staff] welcome ramen\n");
-	pthread_cond_broadcast(&ramen_cond);
-	pthread_mutex_unlock(&kitchen_lock);
-	return NULL;
-}
 int	main()
 {
-	struct s_SharedContext shared_ctx
-	pthread_t	t_chef, t_customer1, t_customer2;
+	struct s_SharedContext shared_ctx;
+	struct s_Dongle dongle_array[2];
+	struct s_Coder coder_array[2];
 
-	pthread_mutex_init(&kitchen_lock, NULL);
-	pthread_cond_init(&ramen_cond, NULL);
+	coder_array[0].number = 1;
+	coder_array[0].left_hand_dongle = &dongle_array[0];
+	coder_array[0].right_hand_dongle = &dongle_array[1];
 
-	pthread_create(&t_customer1, NULL, customer, "Kyaku:A");
-	pthread_create(&t_customer2, NULL, customer, "Kyaku:B");
-	sleep(1);
-	pthread_create(&t_chef, NULL, chef, NULL);
+	coder_array[1].number = 2;
+	coder_array[1].right_hand_dongle = &dongle_array[0];
+	coder_array[1].left_hand_dongle = &dongle_array[1];
 
-	pthread_join(t_customer1, NULL);
-	pthread_join(t_customer2, NULL);
-	pthread_join(t_chef, NULL);
+	shared_ctx.number_of_coders = 2;
+	shared_ctx.time_to_debug = 200;
+	shared_ctx.time_to_refactor = 200;
+	shared_ctx.time_to_compile = 200;
+	shared_ctx.is_active = true;
 
-	pthread_mutex_destroy(&kitchen_lock);
-	pthread_cond_destroy(&ramen_cond);
-	printf("[main]finished sales\n");
+	printf("Nnmber_of_coders:%d\n", shared_ctx.number_of_coders);
+	printf("coder_array[0]:number %d, left_hand_dongle %p, right_hand_dongle %p\n", coder_array[0].number, coder_array[0].left_hand_dongle, coder_array[0].right_hand_dongle);
+	printf("coder_array[1]:number %d, left_hand_dongle %p, right_hand_dongle %p", coder_array[1].number, coder_array[1].left_hand_dongle, coder_array[1].right_hand_dongle);
+	// pthread_t	t_chef, t_customer1, t_customer2;
+	//
+	// pthread_mutex_init(&kitchen_lock, NULL);
+	// pthread_cond_init(&ramen_cond, NULL);
+	//
+	// pthread_create(&t_customer1, NULL, customer, "Kyaku:A");
+	// pthread_create(&t_customer2, NULL, customer, "Kyaku:B");
+	// sleep(1);
+	// pthread_create(&t_chef, NULL, chef, NULL);
+	//
+	// pthread_join(t_customer1, NULL);
+	// pthread_join(t_customer2, NULL);
+	// pthread_join(t_chef, NULL);
+	//
+	// pthread_mutex_destroy(&kitchen_lock);
+	// pthread_cond_destroy(&ramen_cond);
+	// printf("[main]finished sales\n");
 	return (0);
 }
